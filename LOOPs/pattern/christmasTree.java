775323aeb3284c31ef9d70edd4db38a971a8b8c3
Ziyad-213 <ziyadchaudhary78@gmail.com>
2026-05-23 08:01:11 +0530

    Christmas Tree Code

---
+++ LOOPs/pattern/christmasTree.java
@@ -0,0 +1,32 @@
+public class christmasTree{
+    public static void main(String[] args){
+
+
+        for(int i=1;i<=5;i++){
+
+            for(int k=1;k<=5-i;k++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    
+
+    for(int i=1;i<=3;i++){
+        for(int k=1;k<=4;k++){
+            System.out.print(" ");
+        }
+         
+                System.out.print("* ");
+
+    
+    System.out.println();
+}
+}
+}
+
+
+
+     
