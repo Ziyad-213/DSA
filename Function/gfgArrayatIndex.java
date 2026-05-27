03eaad35794b8eed2b0950eb9c3c98945044aa2d
Ziyad-213 <ziyadchaudhary78@gmail.com>
2026-05-27 10:10:54 +0530

    GFG Array Problems

---
+++ Function/gfgArrayatIndex.java
@@ -0,0 +1,14 @@
+public class gfgArrayatIndex{
+    public static void main(String[] args) {
+        int[] arr={1,3,4,6};
+        int i=2;
+
+        if(i<arr.length){
+        System.out.println(arr[i]);
+        }
+        else{
+              System.out.println(-1);
+        }
+        
+    }
+}
